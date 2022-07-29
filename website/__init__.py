4a9cab9b4d1c4efb6ef55e89bff13e63377d8ea2
Simon Yen <simonyen@foxmail.com>
2022-07-29 09:01:12 +0800

    不能暴露自己的mysql数据库地址

--- website/__init__.py
+++ website/__init__.py
@@ -17,7 +17,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Simon Yen'
     # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://SimonYen:yanzengyi123@SimonYen.mysql.pythonanywhere-services.com/SimonYen$blog'
+    app.config['SQLALCHEMY_DATABASE_URI']='something'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.config['SQLALCHEMY_COMMIT_TEARDOWN'] = True
     app.config['MDEDITOR_FILE_UPLOADER'] = path.join(BASEDIR, 'editor_cache')
